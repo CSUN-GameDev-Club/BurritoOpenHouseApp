f8c538fabde1b6c4367aa779f44b1ac09d79c3a0
Yazn <36912068+YaznSarah@users.noreply.github.com>
2021-05-27 10:21:01 -0700

    tiny adjustment

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,7 @@ $('#navbar').on('click', 'a', function (e) {
         document.getElementById("Welcome").style.display = "none";
         // document.getElementById("about").style.visibility = "visible";
         document.getElementById("about").style.display = "block";
-        document.getElementById("Meet the Team").style.display = "none";
+        document.getElementById("Meet-the-Team").style.display = "none";
         document.getElementById("Credits").style.display = "none";
         // pageLeftOn = "A";
         $('#about')
@@ -18,21 +18,21 @@ $('#navbar').on('click', 'a', function (e) {
     else if (this.text == "Home" || this.text == "Project Burrito") {
         document.getElementById("Welcome").style.display = "block";
         document.getElementById("about").style.display = "none";
-        document.getElementById("Meet the Team").style.display = "none";
+        document.getElementById("Meet-the-Team").style.display = "none";
         document.getElementById("Credits").style.display = "none";
         // pageLeftOn = "H";
     }
     else if (this.text == "Meet The Team") {
         document.getElementById("Welcome").style.display = "none";
         document.getElementById("about").style.display = "none";
-        document.getElementById("Meet the Team").style.display = "block";
+        document.getElementById("Meet-the-Team").style.display = "block";
         document.getElementById("Credits").style.display = "none";
         // pageLeftOn = "H";
     }
     else if (this.text == "Credits") {
         document.getElementById("Welcome").style.display = "none";
         document.getElementById("about").style.display = "none";
-        document.getElementById("Meet the Team").style.display = "none";
+        document.getElementById("Meet-the-Team").style.display = "none";
         document.getElementById("Credits").style.display = "block";
         // pageLeftOn = "H";
     }
@@ -44,7 +44,7 @@ $('#navbar').on('click', 'a', function (e) {
 
 document.addEventListener('DOMContentLoaded', function (e) {
     document.getElementById("about").style.display = "none";
-    document.getElementById("Meet the Team").style.display = "none";
+    document.getElementById("Meet-the-Team").style.display = "none";
     document.getElementById("Credits").style.display = "none";
     // if(pageLeftOn == "A")
     // {
